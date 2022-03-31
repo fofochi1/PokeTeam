21e3845ede08aa2d31966f6f0a3e6bcd75a7b57f
Brian Williams <72617884+bcwilliams198@users.noreply.github.com>
2022-03-31 10:38:20 -0400

    Comments + necessary env variables
    Added some comments
    Added environment variables to test manual deployment to Heroku

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
 # pylint: disable= C0114, C0115, C0116, E1101
+# The above is fine, but I'd prefer that the full names are used for less ambiguity (e.g. "missing-module-docstring")
+# I'd also like for us to have justifications regarding the warnings we disable
+
+from os import getenv
+
 import flask
+
+# Just for testing deployment; I used a separate file to fetch all env variables and imported them in other files for convenience
+HOST = getenv("IP", "0.0.0.0")
+PORT = int(getenv("PORT", "8080"))
 
 app = flask.Flask(__name__)
 
@@ -9,4 +18,4 @@ def main():
     return flask.render_template("index.html")
 
 
-app.run(debug=True)
+app.run(debug=True, host=HOST, port=PORT)
