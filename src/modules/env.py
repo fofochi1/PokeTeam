deb73af239bb2a7778a06737f33db8955d79b3f2
Brian <bcwilliams198@gmail.com>
2022-04-01 16:32:56 -0400

    Added "src" and "modules" folders; "blueprints" folder to come soon

---
+++ src/modules/env.py
@@ -0,0 +1,7 @@
+# Missing module docstring needs to be handled
+
+from os import getenv
+
+GOOGLE_CLIENT_ID = getenv("GOOGLE_CLIENT_ID")
+GOOGLE_CLIENT_SECRET = getenv("GOOGLE_CLIENT_SECRET")
+GOOGLE_DISCOVERY_URL = getenv("GOOGLE_DISCOVERY_URL")
